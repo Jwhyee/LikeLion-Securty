679ac217d0f1560fabb9eedbaa0d044116ddcaf9
Jwhyee <kawnsdud@naver.com>
2022-10-05 15:44:22 +0900

    [ADD] - Add ResponseEntity for Success test case2

--- src/main/java/com/ll/exam/securty_exam/app/controller/MemberController.java
+++ src/main/java/com/ll/exam/securty_exam/app/controller/MemberController.java
@@ -1,24 +1,40 @@
 package com.ll.exam.securty_exam.app.controller;
 
 import lombok.Data;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/member")
 public class MemberController {
 
     @PostMapping("/login")
-    public String login(@RequestBody LoginDto loginDto) {
-        return "username : %s, password : %s".formatted(loginDto.getUsername(), loginDto.getPassword());
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+        if (loginDto.isNotValid()) {
+            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+        }
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authentication", "JWT 키");
+        String body = String.valueOf(loginDto);
+
+        return new ResponseEntity<>(body, headers, HttpStatus.OK);
     }
 
     @Data
     public static class LoginDto {
         private String username;
         private String password;
+
+        public boolean isNotValid() {
+            return username == null || username.trim().length() == 0 || password == null || password.trim().length() == 0;
+        }
     }
 
 
