cb201c6c1e412449355784989684ec8c020603e1
Jwhyee <kawnsdud@naver.com>
2022-10-05 13:43:47 +0900

    [ADD] - Add new Project

---
+++ src/main/java/com/ll/exam/securty_exam/SecurtyExamApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.securty_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurtyExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurtyExamApplication.class, args);
+	}
+
+}
