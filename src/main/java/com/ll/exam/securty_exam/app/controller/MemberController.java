212ca57407521cf358bb1800a64f05eac082f210
Jwhyee <kawnsdud@naver.com>
2022-10-05 15:52:43 +0900

    [ADD] - Add Control flow for Success test case3

--- src/main/java/com/ll/exam/securty_exam/app/controller/MemberController.java
+++ src/main/java/com/ll/exam/securty_exam/app/controller/MemberController.java
@@ -1,9 +1,13 @@
 package com.ll.exam.securty_exam.app.controller;
 
+import com.ll.exam.securty_exam.app.domain.Member;
+import com.ll.exam.securty_exam.app.service.MemberService;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,14 +16,27 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/member")
 public class MemberController {
+
+    private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
         if (loginDto.isNotValid()) {
             return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
         }
+
+        Member member = memberService.findByUsername(loginDto.username).orElse(null);
+        if (member == null) {
+            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+        }
+        if (!passwordEncoder.matches(loginDto.getPassword(), member.getPassword())) {
+            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+        }
+
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authentication", "JWT 키");
         String body = String.valueOf(loginDto);
