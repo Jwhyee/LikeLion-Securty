fa91a337dd9d2ecb4c37e50e84a0367f1a437164
Jwhyee <kawnsdud@naver.com>
2022-10-06 14:03:06 +0900

    [ADD] - Add Util, RsData for Simple return

---
+++ src/main/java/com/ll/exam/securty_exam/app/base/RsData.java
@@ -0,0 +1,26 @@
+package com.ll.exam.securty_exam.app.base;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class RsData<T> {
+    private String resultCode;
+    private String msg;
+    private T data;
+
+    public static <T> RsData<T> of(String resultCode, String msg) {
+        return new RsData<>(resultCode, msg, null);
+    }
+
+    public boolean isSuccess() {
+        return resultCode.startsWith("S-1");
+    }
+
+    public boolean isFail() {
+        return !isSuccess();
+    }
+}
