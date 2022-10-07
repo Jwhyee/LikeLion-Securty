efad3c85ce5b617847fb48d5e6ba1c19a3292afe
Jwhyee <kawnsdud@naver.com>
2022-10-07 15:20:58 +0900

    [REFACTOR] - Refactor Member valid message

--- src/main/java/com/ll/exam/securty_exam/app/web/dto/LoginDto.java
+++ src/main/java/com/ll/exam/securty_exam/app/web/dto/LoginDto.java
@@ -6,12 +6,8 @@ import javax.validation.constraints.NotEmpty;
 
 @Data
 public class LoginDto {
-    @NotEmpty("아이디를 입력해주세요.")
+    @NotEmpty(message = "아이디를 입력해주세요.")
     private String username;
-    @NotEmpty("비밀번호를 입력해주세요.")
+    @NotEmpty(message = "비밀번호를 입력해주세요.")
     private String password;
-
-    public boolean isNotValid() {
-        return username == null || password == null || username.trim().length() == 0 || password.trim().length() == 0;
-    }
 }
