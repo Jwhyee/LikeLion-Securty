bc422dc053904f682c1522471c53acd944bc8791
Jwhyee <kawnsdud@naver.com>
2022-10-07 15:20:15 +0900

    [REFACTOR] - Refactor Member valid system

---
+++ src/main/java/com/ll/exam/securty_exam/app/base/exception/GlobalExceptionHandler.java
@@ -0,0 +1,2 @@
+package com.ll.exam.securty_exam.app.base.exception;public class GlobalExceptionHandler {
+}
--- src/main/java/com/ll/exam/securty_exam/app/web/dto/LoginDto.java
+++ src/main/java/com/ll/exam/securty_exam/app/web/dto/LoginDto.java
@@ -2,9 +2,13 @@ package com.ll.exam.securty_exam.app.web.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class LoginDto {
+    @NotEmpty("아이디를 입력해주세요.")
     private String username;
+    @NotEmpty("비밀번호를 입력해주세요.")
     private String password;
 
     public boolean isNotValid() {
